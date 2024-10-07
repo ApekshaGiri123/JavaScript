80f6eb18da864a5afef63f0455e6e7af94858790
Apeksha <apekshagiri2021@gmail.com>
2024-10-07 10:15:40 +0530

    New

---
+++ Basic_1/Variable.js
@@ -0,0 +1,19 @@
+const a=10
+let n="Sony"
+var val=40.3
+console.log(a)
+console.log(n)
+
+
+
+// console.log(val)
+console.log(z);  // undefined
+var z = 10;
+console.log(z);  // 10
+
+console.table({ a, n, val, z });
+
+
+let name="Apula"
+let isLogged=true
+let age=23
