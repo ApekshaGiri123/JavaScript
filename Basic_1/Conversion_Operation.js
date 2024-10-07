c2ab9270efc2351a318a05eb12c681b198fb5ec5
Apeksha <apekshagiri2021@gmail.com>
2024-10-07 10:31:45 +0530

    data

---
+++ Basic_1/Conversion_Operation.js
@@ -0,0 +1,36 @@
+// let score=undefined
+// console.log(score)//undefined
+// console.log(typeof score)//undefined
+// let Num=Number(score) //string convdrt into number
+// console.log(Num)///NaN
+// console.log(typeof Num) //number
+
+
+// let score=null
+// console.log(score)//null
+// console.log(typeof score)//object
+// let Num=Number(score) //string convdrt into number
+// console.log(Num)//0
+// console.log(typeof Num)//number
+
+
+
+
+
+
+
+let score="64 abc"
+console.log(score)//64 abc
+console.log(typeof score)//string
+let Num=Number(score) //string convdrt into number
+console.log(Num)//NaN
+console.log(typeof Num)//number
+
+
+
+// let score="64"
+// console.log(score)//64
+// console.log(typeof score)//string
+// let Num=Number(score) //string convdrt into number
+// console.log(Num)//64
+// console.log(typeof Num)//Number
