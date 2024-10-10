df2bc8ddbb807dcaa61414e0fe1d9d21495acf9e
Apeksha <apekshagiri2021@gmail.com>
2024-10-10 20:13:31 +0530

    function

---
+++ Basic_3/Function.js
@@ -0,0 +1,22 @@
+function fun(){
+    console.log("H")
+    console.log("E")
+    console.log("L")
+    console.log("L")
+    console.log("O")
+}
+fun()
+
+
+// function sum(a,b){//parameter
+//     console.log(a+b)
+// }
+// const res=sum(8,5)//arguments
+// console.log(res)//undefined 
+
+
+function sum(a,b){//parameter
+    return a+b
+}
+const res=sum(8,5)//arguments
+console.log(res)//undefined 
