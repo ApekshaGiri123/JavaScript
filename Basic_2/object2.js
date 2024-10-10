7cb5c38124425e5b5df92a840335cdd123416e9b
Apeksha <apekshagiri2021@gmail.com>
2024-10-10 19:35:58 +0530

    destructuring og=f object &some method

--- Basic_2/object2.js
+++ Basic_2/object2.js
@@ -2,12 +2,15 @@
 // user.id=123
 // user.name="hitedhs"
 // console.log(user)
-
+const mySym=Symbol("Key1")
 user={
     id:123,
     name:{
         name:"hello"
-    }
+    },
+    [mySym]:"Key"
+
+
 }
 console.log(user.name.name)
 
@@ -19,3 +22,41 @@ const obj3={5:"a",6:"b"}
 
 const obj4=Object.assign({},obj1,obj2,obj3)
 console.log(obj4)
+
+
+
+console.log(Object.keys(user))//[ 'id', 'name' ] 
+
+
+console.log(Object.values(user))//[ 123, { name: 'hello' } ]
+
+
+
+console.log(user[mySym])
+
+
+//entries()-> R  eturns an array of key/values of the enumerable own properties of an object
+
+console.log(Object.entries(user))//[ [ 'id', 123 ], [ 'name', { name: 'hello' } ] ]
+
+
+
+//tell us given oject have property or not
+console.log(user.hasOwnProperty("name"))
+
+
+
+//  destructuring the object
+
+const coarse={
+    name:"ABC",
+    price:99,
+
+}
+
+const{name,price}=coarse
+console.log(name,price)
+
+
+const{name:a,price:b}=coarse
+console.log(a,b)
